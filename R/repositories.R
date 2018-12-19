087b797819e66569fe283e48cab0905a950124d5
Eric Bryant <EricEdwardBryant@gmail.com>
2018-12-19 16:37:22 -0500

    Add options for repositories

--- R/repositories.R
+++ R/repositories.R
@@ -1,19 +1,35 @@
 # ---- Repositories ----
+#' Set repositories
+#'
+#' Convenience function for setting repositories.
+#'
+#' @param repos A named character vector of repositories.
+#' Defaults to [rprofile::repos()].
+#'
+#' @md
+#' @export
+
+set_repositories <- function(repos = repos()) {
+  options(repos = repos)
+}
+
 #' Create repository vector
 #'
 #' Create a named vector of repositories
 #'
 #' @param ... User specified repositories added to beginning of vector.
 #' @param cran The CRAN snapshot date ("YYYY-MM-DD").
-#' Defaults to [rprofile::version_cran()].
+#' Defaults to the "rprofile.cran" option if set, else
+#' [rprofile::version_cran()].
 #' @param bioc The Bioconductor version.
-#' Defaults to [rprofile::version_bioc()].
+#' Defaults to the "rprofile.bioc" option if set, else
+#' [rprofile::version_bioc()].
 #' @param cran_mirror URL for CRAN snapshot mirror.
-#' Defaults to
-#' `getOption("rprofile.cran.mirror", "https://cran.microsoft.com/snapshot")`.
+#' Defaults to the "rprofile.cran.mirror" option if set, else
+#' `"https://cran.microsoft.com"`.
 #' @param bioc_mirror URL for Bioconductor mirror.
-#' Defaults to
-#' `getOption("rprofile.bioc.mirror", "https://bioconductor.org")`.
+#' Defaults to the "rprofile.bioc.mirror" option if set, else
+#' `"https://bioconductor.org"`.
 #'
 #' @examples
 #' rprofile::repos()
@@ -30,10 +46,10 @@
 #' @export
 
 repos <- function(...,
-                  cran         = version_cran(),
-                  bioc         = version_bioc(),
+                  cran         = getOption("rprofile.cran", version_cran()),
+                  bioc         = getOption("rprofile.bioc", version_bioc()),
                   cran_mirror  = getOption("rprofile.cran.mirror",
-                                           "https://cran.microsoft.com/snapshot"),
+                                           "https://cran.microsoft.com"),
                   bioc_mirror  = getOption("rprofile.bioc.mirror",
                                            "https://bioconductor.org")) {
   c(...,
@@ -44,7 +60,12 @@ repos <- function(...,
 
 # ---- Repository Utilities ----
 repo_cran <- function(snapshot, mirror) {
-  c(CRAN = paste(mirror, snapshot, sep = "/"))
+  if (snapshot == "latest") {
+    url <- mirror
+  } else {
+    url <- paste(mirror, "snapshot", snapshot, sep = "/")
+  }
+  c(CRAN = url)
 }
 
 
