6b76b1c949dac6e1c74a4318e5cde18775e63e29
Eric Bryant <EricEdwardBryant@gmail.com>
2018-12-27 12:43:14 -0500

    Use .Library for system library

--- inst/templates/Rprofile.R
+++ inst/templates/Rprofile.R
@@ -18,7 +18,7 @@
 }
 
 # Check for rprofile package
-.rprofile_installed <- function(lib = .libPaths()[1]) {
+.rprofile_installed <- function(lib = .Library) {
   is_needed <- function(x, lib) length(find.package(x, lib, quiet = TRUE)) == 0L
   need_remotes  <- is_needed("remotes", lib)
   need_rprofile <- is_needed("rprofile", lib)
